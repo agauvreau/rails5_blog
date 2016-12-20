9577c491d5013aa172571dd777894a0d67a80ca5
Adam Gauvreau <adamgauvreau@hotmail.com>
2016-12-20 01:08:53 +0000

    Added validations to articles

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   # Guard helps to run test, we do not have to repeat a long command everytime
+  # Once installed, in a separate terminal type guard to activate it. 
   gem 'guard', '~> 2.14.0'
   gem 'guard-rspec', '~> 4.7.2' 
   gem 'guard-cucumber', '~> 2.1.2'
--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "Creating Articles" do
     scenario "A user creates a new article" do
-        #visiot root
+        #visit root
         visit "/"
         #method to click on something
         click_link "New Article"
@@ -15,6 +15,21 @@ RSpec.feature "Creating Articles" do
         expect(page).to have_content("Article has been created")
         expect(page.current_path).to eq(articles_path) 
         end
+    #this scenario check article validations    
+    scenario "A user fails to create a new article" do
+        #visit root
+        visit "/"
+        #method to click on something
+        click_link "New Article"
+        #this method fill the element selected with something
+        fill_in "Title", with: ""
+        fill_in "Body", with: ""
+        click_button "Create Article"
+        expect(page).to have_content("Article has not been created")
+        expect(page).to have_content("Title can't be blank")
+        expect(page).to have_content("Body can't be blank")
+    end
+    
 end
     
     
