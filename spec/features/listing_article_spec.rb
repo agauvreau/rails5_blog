52bb0a43510031f51d8327588fe8f8b91b26f071
Adam Gauvreau <adamgauvreau@hotmail.com>
2017-01-07 02:14:11 +0000

    displaying a message when no messages are created

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -17,5 +17,25 @@ RSpec.feature "Listing Articles" do
         expect(page).to have_link(@article2.title)
         
     end
+    
+    scenario "A user has no articles" do
+        #delete all to simulate a User without any articles
+        Article.delete_all
+        
+        visit "/"
+        
+        expect(page).not_to have_content(@article1.title)
+        expect(page).not_to have_content(@article1.body)
+        expect(page).not_to have_content(@article2.title)
+        expect(page).not_to have_content(@article2.body)
+        expect(page).not_to have_link(@article1.title)
+        expect(page).not_to have_link(@article2.title)
+        
+        #checks for a no articles created message
+        within ("h1#no-articles") do
+           expect(page).to have_content("No Article Created")
+        end
+        
+    end
 
 end
