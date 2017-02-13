3bcd5ada6aed9ec158d4644a75577a839f8a1be5
Adam Gauvreau <adamgauvreau@hotmail.com>
2017-02-13 16:16:27 +0000

    Links hidden from non signed in user and non-owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.feature "Showing an article" do
     #before running the scenario we need at least 1 article 
     before do
-        john = User.create(email: "john@example.com", password: "password")
-        login_as(john)
-        @article = Article.create(title: "Title one", body: "Body of article one", user: john)
+        @john = User.create(email: "john@example.com", password: "password")
+        @fred = User.create(email: "fred@example.com", password: "password")
+        @article = Article.create(title: "Title one", body: "Body of article one", user: @john)
     end
     
-    scenario "A user shows an article" do
+    scenario "To non-signed in user hide the Edit and Delete buttons" do
         visit "/"
         click_link @article.title
         
@@ -17,7 +17,25 @@ RSpec.feature "Showing an article" do
         expect(page).to have_content(@article.title)
         #checks if the path is what is should be 
         expect(current_path).to eq(article_path(@article))
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+
+    end
+    
+    scenario "To non-owner user hide the Edit and Delete buttons" do
         
+        login_as(@fred)
+        visit "/"
+        click_link @article.title
+        
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(page).to have_content(@article.title)
+        #checks if the path is what is should be 
+        expect(current_path).to eq(article_path(@article))
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+
     end
     
     
